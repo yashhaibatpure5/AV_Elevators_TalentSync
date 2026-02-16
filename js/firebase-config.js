6d03896d1ce6be83973f562345f3b14fa6efbe82
Haibatpure Yashwant Uttam <yashhaibatpure5@gmail.com>
2026-02-16 12:41:24 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyB8yT4JpbpKVrE59UnRDYbMWO6QjyguAfo",
   authDomain: "av-elevators-33e0f.firebaseapp.com",
   projectId: "av-elevators-33e0f",
   storageBucket: "av-elevators-33e0f.firebasestorage.app",
   messagingSenderId: "534356263711",
-  appId: "1:534356263711:web:f33b3dde9618ce056d662a"
+  appId: "1:534356263711:web:f33b3dde9618ce056d662a",
 };
 
 const app = initializeApp(firebaseConfig);
-
-// 🔥 Connect Firestore database
 const db = getFirestore(app);
 
-// Export DB so other JS files can use it
 export { db };
